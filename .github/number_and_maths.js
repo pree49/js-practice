8ce2766e945fa813a6a7f6f0db58bbbfbd9060ed
pree49 <157723086+pree49@users.noreply.github.com>
2024-01-26 18:25:25 +0000

    container config done

---
+++ .github/number_and_maths.js
@@ -0,0 +1,11 @@
+const score = 400
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString());
+console.log(typeof balance.toString());
+
+//for fixing the precision
+
+console.log(balance.toFixed(2))
