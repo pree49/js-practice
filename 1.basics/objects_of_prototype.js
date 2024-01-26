80fc0cdb444d803526366788a58c5736f8acdd72
pree49 <157723086+pree49@users.noreply.github.com>
2024-01-26 18:09:55 +0000

    container config done

--- 1.basics/objects_of_prototype.js
+++ 1.basics/objects_of_prototype.js
@@ -19,6 +19,71 @@ const gameName = new String('hi preeeti')
 
 // console.log(gameName.indexOf('t'))
 
+//try to learn more methods of prototype
+
+// const newString = gameName.substring(0 , 4)  //note one thing the value which you have included in the last
+//is not gets included
+
+// console.log(newString)
+
+// const anotherString = gameName.slice(-5 , 7)
+// console.log(anotherString) //in this string gets printed from last 
+//values.
+
+//in substring we cant start values from negative 
+
+//values will be only from zeroes
+
+//method for removing spaces
+
+const newString = "  preeti  ";
+console.log(newString);
+console.log(newString.trim()); 
+
+//now string with removed spaces gets printed
+
+//for replacing the words
+
+const url="https://preeti.com/preeti%12shukla"
+
+console.log(url.replace('%12','-'))
+
+//strings are very useful for knowing so read it carefully
+
+//in any languages strings are most powerful
+
+//with the help of the word we also comes to know that any word is present in the 
+//string or not-->answer comes out in true 
+//and false form
+
+console.log(url.includes('preeti'))
+
+const preetiuser = new String('nonnu-papa-mumma-preeti')
+
+//if we want to convert the string on the basis of the 
+//-in arrays then we can also perform this task
+
+console.log(preetiuser.split('-'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
